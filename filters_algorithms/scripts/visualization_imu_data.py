5890e617eabcd8ac7d6927becf21b8da0810b27c
Love Panta <54012619+love481@users.noreply.github.com>
2021-07-09 22:43:12 +0545

    Update visualization_imu_data.py

--- filters_algorithms/scripts/visualization_imu_data.py
+++ filters_algorithms/scripts/visualization_imu_data.py
@@ -4,10 +4,10 @@ import rospy
 from std_msgs.msg import Float32
 import tf
 import time
-t=[]
-yaw=[]
-Ryaw=[]
-rt=[]
+filtered_t=[]
+filtered_yaw=[]
+raw_yaw=[]
+raw_t=[]
 plt.ion()
 figure, ax = plt.subplots()
 ax.set_title('yaw_plot')
@@ -15,32 +15,26 @@ ax.axis([0,100000,-180,180])
 class IMU_visualize:
     def __init__(self):
         rospy.init_node('imu_visualize', anonymous=True)
-        rospy.Subscriber("IMU/yaw",Float32, self.callback)
-        rospy.Subscriber("IMU/Raw_yaw",Float32, self.callbackR)
-        self.prev=Float32()
-        self.curr=Float32()
-        self.prev.data=0
-        self.curr.data=0
+        rospy.Subscriber("IMU/Raw_yaw",Float32, self.Raw_callback)
+        rospy.Subscriber("IMU/yaw",Float32, self.Filtered_callback)
         self.prevTime=rospy.Time().now()
-        self.dt=0
+        self.fdt=0
         self.rdt=0
-    def callback(self,data):
-            #rospy.loginfo("%lf",self.curr.data)
-            self.curr.data=data.data
-            yaw.append(self.curr.data)
-            self.dt=self.dt+(rospy.Time().now()-self.prevTime).to_sec()
-            t.append(self.dt)
-    def callbackR(self,data):
-            #rospy.loginfo("%lf",self.curr.data)
-            self.prev.data=data.data
-            Ryaw.append(self.prev.data)
+    def Raw_callback(self,data):
+            #rospy.loginfo("%lf",self.data.data)
+            raw_yaw.append(data.data)
             self.rdt=self.rdt+(rospy.Time().now()-self.prevTime).to_sec()
-            rt.append(self.rdt)
+            raw_t.append(self.rdt)
+    def Filtered_callback(self,data):
+            #rospy.loginfo("%lf",self.curr.data)
+            filtered_yaw.append(data.data)
+            self.fdt=self.fdt+(rospy.Time().now()-self.prevTime).to_sec()
+            filtered_t.append(self.fdt)
     def run(self):
         while not rospy.is_shutdown():
             #plt.plot([self.curr.x,self.prev.x],[self.curr.y,self.prev.y],'r',linewidth=1)
-            plt.plot(t,yaw,'-b',linewidth=0.9)
-            plt.plot(rt,Ryaw,'-r',linewidth=0.9)
+            plt.plot(filtered_t,filtered_yaw,'-b',linewidth=0.9)
+            plt.plot(raw_t,raw_yaw,'-r',linewidth=0.9)
             figure.canvas.draw()
             figure.canvas.flush_events()
             time.sleep(0.01)
